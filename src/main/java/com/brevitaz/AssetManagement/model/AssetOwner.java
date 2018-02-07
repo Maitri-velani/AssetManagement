5f35499f75c21fcd7f6919a8c8a7b253987bbf00
Admin <Admin@Acer>
2018-02-07 17:05:42 +0530

    New Change

---
+++ src/main/java/com/brevitaz/AssetManagement/model/AssetOwner.java
@@ -0,0 +1,31 @@
+package com.brevitaz.AssetManagement.model;
+
+public class AssetOwner {
+	private String ownerId;
+	private String firstName;
+	private String lastName;
+	
+	public String getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(String ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLasttName() {
+		return lastName;
+	}
+	public void setLastName(String lasttName) {
+		this.lastName = lasttName;
+	}
+	
+	@Override
+	public String toString() {
+		return "AssetOwner [ownerId=" + ownerId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+}
